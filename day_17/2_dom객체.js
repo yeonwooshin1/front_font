e3391806cdd3cb85bcb5d6cf731e96c4df8ef9b8
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-06-19 14:27:45 +0900

    오전 끝

---
+++ day_17/2_dom객체.js
@@ -0,0 +1,75 @@
+
+
+// 1.
+
+const title = document.querySelector('#title');
+title.innerHTML = 'DOM 조작 성공!';
+
+// 2.
+
+function func2(){
+    console.log('func2');
+    const input = document.querySelector('#usernameInput');
+    const text = input.value;
+    const greeting = document.querySelector('#greeting');
+    greeting.innerHTML = '안녕하세요, ' + text+ '님!';
+}
+
+// 3.
+
+function func3(){
+    console.log('func3');
+    const box = document.querySelector('#colorBox');
+    box.style = 'background-color : skyblue; color : white;'
+}
+
+// 4.
+
+function func4(){
+    console.log('func4');
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+}
+
+// 5.
+function func5(){
+    console.log('func5');
+    const box = document.querySelector('#targetBox');
+    box.remove();
+}
+
+// 6.
+const itemAll = document.querySelectorAll('.item');
+for(let i = 0 ; i < itemAll.length ; i++){
+    itemAll[i].style = 'color : green; font-weight : bold;';
+}
+
+// 7.
+
+function func7(){
+    console.log('func7');
+    const imageChange = document.querySelector('#mainImage');
+    imageChange.src = 'https://placehold.co/600x400/red/white';
+}
+
+// 8.
+function func8(){
+    const foodSelect = document.querySelector('#foodSelect');
+    const text = foodSelect.value;
+    console.log(text);
+}
+
+//9.
+let array1 = [];
+function func9(){
+
+    const input = document.querySelector('#itemInput');
+    const array = document.querySelector('#arrayStatus');
+
+    if(input.value == '') return ;
+    
+    array1.push(input.value);
+    array.textContent = `[ ${array1} ]`;
+
+    input.value = '';
+}
--- day_17/dom객체.js
+++ day_17/dom객체.js
@@ -24,21 +24,30 @@
 
         4. dom객체.innerHTML                        : 마크업내 html 내용물을 호출하는 속성
                 * <마크업> !!!여기가 innerHTML 입니다!!! </마크업>
+        
         5. dom객체.value
             * <마크업 value="여기!!" / >
             * value있음 : <input>, <select>, <textarea>, <radio> 등등
             * value없음 : <div> , <span> , <table> 등등
-            *  
-
+        
+        6. dom객체.stlye                            : 마크업내 stlye(CSS) 요소 대입
+        
+        7. dom객체.stlye.CSS 속성명                 : 마크업내 style(CSS) 요소 추가
+        
+        8. dom객체.classList                        : 마크업내 적용된 class 들 반환
+            dom객체.classList.add('클래스명')           : 마크업내 클래스 추가
+            dom객체.classList.remove('클래스명')        : 마크업내 클래스 삭제
+            dom객체.classList.toggle('클래스명')        : 마크업내 지정클래스가 없으면 추가 있으면 삭제
+            dom객체.classList.contains('클래스명')       마크업내 지정클래스가 있는지 확인 , true / false 반환
 
 */
-//[1]
+// [1]
 console.log(document);
 
-//[2]
+// [2]
 document.write('내장객체 함수');
 
-//[3]
+// [3]
 const div = document.querySelector('div');
 console.log( div );
 const box2 = document.querySelector('.box2')
@@ -46,21 +55,21 @@ console.log( box2 );
 const box3 = document.querySelector('#box3');
 console.log( box3 );
 
-//[4]
+// [4]
 const divArray = document.querySelectorAll('div'); // 여러개
 console.log( divArray );
 
-//[5]
+// [5]
 const div2 = document.querySelector('div');
 const html = div2.innerHTML;
 console.log( html ); // <div> 박스1 </div> ---> 박스1
 
-//[6]
+// [6]
 // 함수 실행 조건 : 함수1실행 버튼을 클릭했을 때
 // 매개변수 : x 리턴값 : x 로직 : console.log 출력
 function func1(){ //함수선언
     // 1. 함수실행테스트출력
-    console.log('---func1 exe---')
+    console.log('---func1 exe---');
     // 2. input 마크업을 JS dom객체 가져오기
     const myInput = document.querySelector('.myInput');
         console.log( myInput ); // 확인
@@ -69,8 +78,22 @@ function func1(){ //함수선언
         console.log( text );
 } 
 
-//[7]
+// [7]
 function func2(){
     console.log('---func2 exe---');
-    document.querySelector('.title').innerHTML = 'JS에서 작성한것'
+    document.querySelector('.title').innerHTML = 'JS에서 작성한것';
+}
+
+// [8]
+function func3(){
+    console.log('---func3 exe---');
+    // 1. 특정한 선택자의 마크업을 가져오기
+    const title2 = document.querySelector('.title2');
+    // 2. 해당 마크업 객체내 .style속성
+    title2.style = 'color : red; font-size : 10px;' // css 문법 문자열로 대입 
+
+    //*
+    const body = document.querySelector('body');
+    body.style.backgroundColor = 'gray';
+    title2.classList.toggle('myStyle');
 }
